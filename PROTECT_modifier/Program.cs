625e78df48711995f894dfb2c5588ff561af5da1
Alona <122108963+yelloally@users.noreply.github.com>
2023-03-03 16:28:25 +0100

    Add files via upload

---
+++ PROTECT_modifier/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Person
+{
+    protected string name;
+    protected int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    protected void Greet()
+    {
+        Console.WriteLine("Hello, my name is {0} and I am {1} years old.", name, age);
+    }
+}
+
+public class Student : Person
+{
+    private string school;
+
+    public Student(string name, int age, string school) : base(name, age)
+    {
+        this.school = school;
+    }
+
+    public void Learn()
+    {
+        Greet(); // Can call the protected Greet() method in the base class
+        Console.WriteLine("I am a student of {0}.", school);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student myStudent = new Student("Alona", 17, "DSW");
+        myStudent.Learn();
+    }
+}
